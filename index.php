5ecde3d23e03dc88ab388ddf4ae2181d8f768161
salomee453 <72232793+salomee453@users.noreply.github.com>
2020-10-03 00:58:39 +0800

    se crea index

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pasteleria bellarose</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body  class="bg-dark">
+
+
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+  <img src="img/dona.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+            pasteleria bellarose
+  </a>
+    
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </form>
+  </div>
+</nav>
+  </div>
+</nav>
+
+     </header>
+     <main>
+       
+       <div class="container text-white">
+       <div class="row justify-content-center mt-4">
+       <div class= "col-4">
+       <form>
+       <h3 class="text center mb-4">factura de compra</h3>
+       <div class="row mb-3">
+    <div class="col">
+         <input type="text" class="form-control" placeholder="producto1">
+            </div>
+             <div class="col">
+                   <input type="number" class="form-control" placeholder="precio($)">
+               </div>
+               </div>
+               <div class="row mb-3">
+    <div class="col">
+         <input type="text" class="form-control" placeholder="producto2">
+            </div>
+             <div class="col">
+                   <input type="number" class="form-control" placeholder="precio($)">
+               </div>
+               </div>
+               <div class="row mb-3">
+    <div class="col">
+         <input type="text" class="form-control" placeholder="producto3">
+            </div>
+             <div class="col">
+                   <input type="number" class="form-control" placeholder="precio($)">
+               </div>
+               </div>
+               <div class="row mb-3">
+    <div class="col">
+         <input type="text" class="form-control" placeholder="producto4">
+            </div>
+             <div class="col">
+                   <input type="number" class="form-control" placeholder="precio($)">
+               </div>
+               </div>
+               <div class="row mb-3">
+    <div class="col">
+         <input type="text" class="form-control" placeholder="producto5">
+            </div>
+             <div class="col">
+                   <input type="number" class="form-control" placeholder="precio($)">
+               </div>
+               </div>
+               <button type="submit" class="btn btn-primary btn-block">calcular</button>
+
+            </form>
+          </forms>
+       </div>
+
+       </div>
+
+     </div>
+
+     </main>
+     <footer>
+     
+     
+     
+     </footer>
+
+     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</body>
+</html>
